3b3888c02a36bad6cb7b497f5104514e8fb7dddf
enzotoyos <enzo.toyos@gmail.com>
2022-01-18 22:12:20 +0100

    modif

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,6 +1,5 @@
-
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View } from "react-native";
 
 import {
   Layout,
@@ -11,12 +10,26 @@ import {
   useTheme,
 } from "react-native-rapi-ui";
 import firebase from "firebase";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function ({ navigation }) {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const { isDarkmode, setTheme } = useTheme();
+
+  const getData = async () => {
+    try {
+      const uid = await AsyncStorage.getItem("uid");
+      console.log(uid);
+    } catch (e) {
+      // error reading value
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <Layout>
       <View style={styles.container}>
@@ -27,86 +40,72 @@ export default function ({ navigation }) {
         />
         <Text style={styles.button}>{String(name)}</Text>
 
-	useEffect(() => {
-		getData()
-	}, []);
+        <TextInput
+          containerStyle={{ marginTop: 15 }}
+          placeholder="mail"
+          value={email}
+          autoCapitalize="none"
+          autoCompleteType="off"
+          autoCorrect={false}
+          keyboardType="email-address"
+          onChangeText={(text) => setEmail(text)}
+        />
+        <TextInput
+          containerStyle={{ marginTop: 15 }}
+          placeholder="identifiant"
+          value={name}
+          autoCapitalize="none"
+          autoCompleteType="off"
+          autoCorrect={false}
+          onChangeText={(text) => setName(text)}
+        />
 
-	const getData = async () => {
-		try {
-			const uid = await AsyncStorage.getItem('uid');
-			console.log(uid);
-		} catch (e) {
-			// error reading value
-		}
-	}
-	const [email, setEmail] = useState("")
-	const [name, setName] = useState("")
-
-	return (
-		<Layout>
-			<View style={styles.container}>
-				<Avatar
-					source={require('../../media/avatar.png')}
-					size="xl"
-					shape="round"
-				/>
-				<Text
-					style={styles.button}>
-					{String(name)}
-				</Text>
-
-				<TextInput
-					containerStyle={{ marginTop: 15 }}
-					placeholder="mail"
-					value={email}
-					autoCapitalize="none"
-					autoCompleteType="off"
-					autoCorrect={false}
-					keyboardType="email-address"
-					onChangeText={(text) => setEmail(text)}
-				/>
-				<TextInput
-					containerStyle={{ marginTop: 15 }}
-					placeholder="identifiant"
-					value={name}
-					autoCapitalize="none"
-					autoCompleteType="off"
-					autoCorrect={false}
-					onChangeText={(text) => setName(text)}
-				/>
-
-				<Button
-					text="Modifier mon mot de passe"
-					onPress={() => {
-						getData();
-					}}
-					style={styles.button}
-				/>
-				<Button
-					text="Modifier mon Avatar"
-					onPress={() => {
-						//
-					}}
-					style={styles.button}
-				/>
-				<Button
-					text="Modifier mes informations"
-					onPress={() => {
-						//
-					}}
-					style={styles.button}
-				/>
-				<Button
-					status="danger"
-					text="Déconnexion"
-					onPress={() => {
-						firebase.auth().signOut();
-					}}
-					style={styles.button}
-				/>
-			</View>
-		</Layout>
-	);
+        <Button
+          text="Modifier mon mot de passe"
+          onPress={() => {
+            getData();
+          }}
+          style={styles.button}
+        />
+        <Button
+          text="Modifier mon Avatar"
+          onPress={() => {
+            //
+          }}
+          style={styles.button}
+        />
+        <Button
+          text="Modifier mes informations"
+          onPress={() => {
+            //
+          }}
+          style={styles.button}
+        />
+        <Button
+          status="danger"
+          text="Déconnexion"
+          onPress={() => {
+            firebase.auth().signOut();
+          }}
+          style={styles.button}
+        />
+        <Button
+          text={isDarkmode ? "Light Mode" : "Dark Mode"}
+          status={isDarkmode ? "success" : "warning"}
+          onPress={() => {
+            if (isDarkmode) {
+              setTheme("light");
+            } else {
+              setTheme("dark");
+            }
+          }}
+          style={{
+            marginTop: 10,
+          }}
+        />
+      </View>
+    </Layout>
+  );
 }
 
 const styles = StyleSheet.create({
